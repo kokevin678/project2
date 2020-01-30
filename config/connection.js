4e0b5142e48594f02ed3dae28164d08b8456cdcc
Chengyu Ko <kokevin678@gmail.com>
2020-01-29 20:44:29 -0800

    try to fix herpko

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+var mysql = require("mysql");
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "monopoly_db"
+  });
+}
+
+connection.connect();
+module.export = connection;
